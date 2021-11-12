44e8c622729f429426e3693c63cc526cac69192c
nieltorres <niel.torres08@gmail.com>
2021-11-12 02:00:04 +0100

    feature: Aplicación simple que simula un listado de cosas por hacer en react native

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+type movie = {
+  name: string,
+  available: number
+}
+
+type moviesType = Array<movie>;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'simpleAppMovies';
+
+  title: string = 'Películas';
+  buttonAvailable: boolean = false;
+
+  movies: moviesType = [
+    { name: 'Vengadores', available: 3 },
+    { name: 'Campeones', available: 2 }
+  ];
+
+
+  constructor() {
+    console.log(this.movies);
+  }
+
+  removeMovie(movie: movie) {
+    movie.available -= 1;
+  }
+
+  addMovie(movie: movie) {
+    movie.available += 1;
+  }
+
+
+
 }
+
+
